5d71b845efd1066cd3ac9108d96912afca688973
nafi bako <nafibako@yahoo.com>
2020-12-12 13:47:40 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+
+    // Example queryURL for Giphy API
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=xlOEhlA4MKejem1J5UfNV0eCqBjPjVUH&q=good job&limit=25&offset=0&rating=g&lang=en";
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+      console.log(response);
+    });
+
+
+
+    function check() {
+        var a=0;
+        var q1=document.quiz.question1.value;
+        var q2=document.quiz.question2.value;
+        var q3=document.quiz.question3.value;
+        var q4=document.quiz.question4.value;
+        var result=document.getElementById('result');
+        var quiz=document.getElementById("quiz");
+        if (q1=="4") {a++}
+        if (q2=="5") {a++}
+        if (q3=="8") {a++}
+        if (q4=="6") {a++}
+        quiz.style.display="none";
+        if (a<=3) {
+          result.textContent=`Your result is ${a}. Sorry you only got 2 correct try again` 
+      } else {
+          result.textContent=`Your result is ${a}. You got 4 correct. Awesome! Great Job!` 
+      }
+      }
+ 
